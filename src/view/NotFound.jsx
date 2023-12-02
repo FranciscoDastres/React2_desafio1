4fc7007159447e35bea483e05be7d03467d6b8ca
FranciscoDastres <franciscomeza.1234@gmail.com>
2023-12-01 21:55:52 -0300

    fixing 404

--- src/view/NotFound.jsx
+++ src/view/NotFound.jsx
@@ -3,7 +3,7 @@ import "./NotFound.css"
 const NotFound = () => {
   return (
     <div>
-        <h1>Error 404</h1>
+        <h1></h1>
         <img className='error-404' 
           src={error404} 
           alt="error-404" 
